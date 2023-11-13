b94c0d2d4da905de4b1a761ba21d8b9bddc5c513
brianm1983 <brianmpetersen@outlook.com>
2023-11-12 23:00:55 -0700

    Just adding nots on JS Fundamentals

---
+++ javascritp.js
@@ -0,0 +1,80 @@
+//let hand = 5;
+//let startPosition = "high-";
+
+//console.log(startPosition + hand);
+
+//let hwllo = "Hello World";
+
+//hello = " Hi there, I changed my value from my original form";
+//console.log(hello)
+
+//const myBirthday = '03.20.1983';
+//alert(myBirthday);
+
+// console.log(23 + 97);
+
+// console.log(3 + 6 + 9 + 3 + 6 + 9)
+
+// console.log((4 + 6 + 9) / 77)
+
+// let a = 10;
+
+// console.log(a);
+
+// console.log(9 * a);
+
+// let b = 7 * a;
+
+// console.log(b);
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
+
+// let x = 100 + 50 *3;
+
+// let x = 123 * Math.pow(10,5);
+// console.log(x);
+
+// let x = (0.2 * 10 + 0.1 * 10) /10;
+// console.log(x);
+
+// let x = "100"
+// let y = "10";
+// let z = x / y;
+// console.log(z);
+
+//NaN-Not A Number
+// let x = 100 / "Apple";
+// console.log(isNaN(x));
+
+// //Infinity example
+// let myNumber = 2;
+// while (myNumber != Infinity) {
+//     console.log(myNumber = myNumber * myNumber);
+// }
+
+// let x = 2 / 0;
+// let y =-2 / 0;
+// console.log(x,y);
+
+// Hexadecimal
+// let x = 0xFF;
+// console.log(x);
+
+// let myNumber = 32;
+// console.log(myNumber.toString(32));
+// console.log(myNumber.toString(16));
+// console.log(myNumber.toString(12));
+// console.log(myNumber.toString(10));
+// console.log(myNumber.toString(8));
+// console.log(myNumber.toString(2));
+
+// let x = 123;
+// let y = new Number(123);
+// console.log(y,x);
+
+// let x = 300;
+// let y = 359;
+// console.log(x === y);
+
